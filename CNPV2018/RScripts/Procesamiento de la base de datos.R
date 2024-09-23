23f4c66f2e423278ab1445dfbe7e83a39d45aa41
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-09-22 20:26:30 -0500

    Procesamiento de base de datos

--- CNPV2018/RScripts/Procesamiento de la base de datos.R
+++ CNPV2018/RScripts/Procesamiento de la base de datos.R
@@ -65,8 +65,7 @@ CNPV2018_MGN_A2_05001<-CNPV2018_MGN_A2_05001 %>%
     COD_MANZANA=paste0(
       U_DPTO,U_MPIO,UA_CLASE,U_SECT_RUR,U_SECC_RUR,UA2_CPOB,U_SECT_URB,U_SECC_URB,U_MZA),
     skVivienda=paste0(COD_ENCUESTAS,U_DPTO,U_MPIO,UA_CLASE,U_EDIFICA,U_VIVIENDA)
-    
-  )
+    )
 
 
 #Construcción de variables de la vivienda
@@ -82,25 +81,108 @@ CNPV2018_1VIV_A2_05001<-CNPV2018_1VIV_A2_05001 %>%
   ) %>% 
   left_join(COD_MANZANA,by = "skVivienda")
 
+
 #Construcción de variables de las personas
+DimNivelEducativo<-read.csv2("https://raw.githubusercontent.com/Esteban7777/Inferencia-Causal/refs/heads/main/CNPV2018/Data/DimNivelEducativo.csv")
+
 
 CNPV2018_5PER_A2_05001<-CNPV2018_5PER_A2_05001 %>% 
   mutate(
-    
     flagMigranteInternacional=case_when(PA_LUG_NAC == 3 ~ 1, .default = 0),
-    
     flagMigranteIntermunicipal=case_when(PA_LUG_NAC == 2 ~ 1, .default = 0),
-    
     flagPET=case_when(P_EDADR > 3 ~ 1, .default = 0),
-    
     flagOcupado=case_when(
       P_TRABAJO == 1 ~ 1,
       P_TRABAJO == 2 ~ 1,
       P_TRABAJO == 3 ~ 1, .default = 0),
-    
     flagDesempleado= case_when(P_EDADR > 3 & P_TRABAJO== 4 ~1, .default = 0),
-    
     flagInactivo=case_when(P_EDADR > 3 & P_TRABAJO> 4 ~1,
-                           P_EDADR > 3 & P_TRABAJO== 0 ~1,.default = 0)
-    educationYears=
+                           P_EDADR > 3 & P_TRABAJO== 0 ~1,.default = 0),
+    skNivelEducativo=P_NIVEL_ANOSR
+    ) %>%
+  left_join(DimNivelEducativo,by = "skNivelEducativo") %>% 
+  left_join(COD_MANZANA,by = "skVivienda")
+
+#Construcción de las variables del hogar
+ViviendaHogar<-CNPV2018_1VIV_A2_05001 %>% 
+  select(skViviendaHogar,
+         flagParedesPrecarias,
+         flagPisosPrecarios,
+         flagFaltaAcueducto,
+         flagFaltaAlcantarillado,
+         estratoNumerico,
+         COD_MANZANA)
+
+CNPV2018_2HOG_A2_05001<-CNPV2018_2HOG_A2_05001 %>% 
+  left_join(ViviendaHogar, by="skViviendaHogar") %>% 
+  mutate(
+    personasHabitacion=case_when(H_NRO_DORMIT==99 ~ NA,
+                                 .default = H_NRO_DORMIT/HA_TOT_PER),
+    flagHacinamiento= case_when(personasHabitacion>3 ~1, .default = 0),
+    precariedadVivienda=flagParedesPrecarias + 
+      flagPisosPrecarios + 
+      flagFaltaAcueducto +
+      flagFaltaAlcantarillado +
+      flagHacinamiento
+      )
+  
+
+#Agregación a nivel de manzana
+#En variables de personas
+variablesPersonas<-CNPV2018_5PER_A2_05001 %>%
+  group_by(COD_MANZANA) %>% 
+  summarise(
+    POBLACION=n_distinct(skPersona),
+    PET=sum(flagPET),
+    Inactivo=sum(flagInactivo),
+    Desempleados=sum(flagDesempleado),
+    Ocupados=sum(flagOcupado),
+    MigrantesInternacionales=sum(flagMigranteInternacional),
+    MigrantesIntermunicipales=sum(flagMigranteIntermunicipal),
+    EducacionPromedio=mean(Años_acumulados,na.rm = TRUE)
+  ) %>% 
+  mutate(
+    PEA=Desempleados+Ocupados,
+    TasaDesempleo=case_when(PEA==0 ~0, .default = Desempleados/PEA),
+    PorcentajeMigrantesInternacionales=MigrantesInternacionales/POBLACION,
+    PorcentajeMigrantesIntermunicipales=MigrantesIntermunicipales/POBLACION
   )
+
+#En variables de viviendas
+variablesViviendas<-CNPV2018_1VIV_A2_05001 %>% 
+  group_by(COD_MANZANA) %>% 
+  summarise(
+    CantidadViviendas=n_distinct(skVivienda),
+    ParedesPrecarias=sum(flagParedesPrecarias),
+    PisosPrecarios=sum(flagPisosPrecarios),
+    PrecariedadAcueducto=sum(flagFaltaAcueducto),
+    PrecariedadAlcantarillado=sum(flagFaltaAlcantarillado),
+    EstratoPromedio=mean(estratoNumerico,na.rm = TRUE)
+  ) %>% 
+  mutate(
+    PorcentajeParedesPrecarias=ParedesPrecarias/CantidadViviendas,
+    PorcentajePisosPrecarios=PisosPrecarios/CantidadViviendas,
+    PorcentajePrecariedadAcueducto=PrecariedadAcueducto/CantidadViviendas,
+    PorcentajePrecariedadAlcantarillado=PrecariedadAlcantarillado/CantidadViviendas 
+  )
+
+#En variables de hogar
+variablesHogares<-CNPV2018_2HOG_A2_05001 %>% 
+group_by(COD_MANZANA) %>% 
+  summarise(
+  CantidadHogares=n_distinct(skHogar),  
+  PersonasHabitacion=mean(personasHabitacion,na.rm = TRUE),
+  HogaresHacinados=sum(flagHacinamiento),
+  PrecariedadPromedio=mean(precariedadVivienda,na.rm = TRUE)
+  ) %>% 
+  mutate(
+    HogaresHacinados=HogaresHacinados/CantidadHogares,
+  )
+
+#Unir variables
+
+ManzanasConsolidado<-variablesPersonas %>% 
+  left_join(variablesHogares,by = "COD_MANZANA") %>% 
+  left_join(variablesViviendas, by = "COD_MANZANA")
+
+
