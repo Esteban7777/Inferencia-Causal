24843e754888b63970dd7804c4ba62065b3137b0
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-10-14 19:00:24 -0500

    RRD

--- Inferencia_causal/RRD.R
+++ Inferencia_causal/RRD.R
@@ -1,41 +1,22 @@
-
 #Cargar los datos y paquetes
 setwd("C:/Users/HP-Laptop/OneDrive - Universidad de Antioquia/Maestría en Economía/Evaluación de impacto/Inferencia-Causal")
 load("Inferencia_causal/estratos_2y3_rdd.RData")
-library(rdrobust)
-library(rdd)
-
-
-#Exploración de los datos
 
 #Tratamiento
 table(estratos_2y3_rdd$treat)
 
-#Variable de asignación
-hist(estratos_2y3_rdd$score)
+#Test de Mccrary
+library(rddensity)
+mccrary_test <- rddensity(estratos_2y3_rdd$score)
+summary(mccrary_test)
+#P valor >0.005 no se rechaza la hipotesis de continuidad por lo que se cumple el supuesto no sorting
 
-#Dependientes
-hist(estratos_2y3_rdd$viv_informal)
+#Se identifica si el diseño es nítido o difuso 
+ggplot(estratos_2y3_rdd, aes(x = score, y =treat )) +
+  geom_point(alpha = 0.3) + 
+  labs(title = "Probabilidad de ser estrato 1 dado el Score",
+       x = "Score", y = "Probabilidad de ser tratado") +
+  theme_minimal()
+#Se identifica un diseño nítido
 
-hist(estratos_2y3_rdd$pop_dens)
-
-hist(estratos_2y3_rdd$hous_dens)
-
-hist(estratos_2y3_rdd$comercial)
-
-hist(estratos_2y3_rdd$accs_c)
-
-hist(estratos_2y3_rdd$migran_5)
-
-#Exploración visual de la discontinuidad de las variables
-plot(estratos_2y3_rdd$score,estratos_2y3_rdd$viv_informal)
-plot(estratos_2y3_rdd$score,estratos_2y3_rdd$pop_dens)
-plot(estratos_2y3_rdd$score,estratos_2y3_rdd$hous_dens)
-plot(estratos_2y3_rdd$score,estratos_2y3_rdd$comercial)
-plot(estratos_2y3_rdd$score,estratos_2y3_rdd$accs_c)
-plot(estratos_2y3_rdd$score,estratos_2y3_rdd$migran_5)
-
-#Test de McCrary
-DCdensity(runvar = estratos_2y3_rdd$score,cutpoint = 0)
-
-
+#
