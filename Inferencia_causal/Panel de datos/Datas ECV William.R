f0ebeb7373626371a58a579e3c275a9c98911e0a
William Alexander Aguirre Antolínez <w.alexanderaguirre@gmail.com>
2024-12-11 20:22:35 -0500

    Update data 2021

--- Inferencia_causal/Panel de datos/Datas ECV William.R
+++ Inferencia_causal/Panel de datos/Datas ECV William.R
@@ -1,6 +1,3 @@
-#2022
-names(ECV_2022)
-table(ECV_2022$hogar)
 variables<-c(
   "comuna",
   "year",
@@ -22,7 +19,11 @@ variables<-c(
   "idHogar",
   "idPersona",
   "hogares"
-  )
+)
+
+#2022
+names(ECV_2022)
+table(ECV_2022$hogar)
 
 data2022<- ECV_2022 %>% 
   mutate(
@@ -57,3 +58,44 @@ data2022<- ECV_2022 %>%
 data2022<-data2022 %>% select(variables)
 
 writexl::write_xlsx(data2022,"C:/Users/HP-Laptop/OneDrive - Universidad de Antioquia/Maestría en Economía/Evaluación de impacto/Inferencia-Causal/Inferencia_causal/Panel de datos/2022.xlsx")
+
+
+#2021
+names(ECV_2021)
+table(ECV_2021$P_016)
+
+data2021<- ECV_2021 %>% 
+  mutate(
+    comuna = as.numeric(P_006),
+    year = 2021,
+    ingreso = as.numeric(P_269),
+    jefe = ifelse(P_016=="1",1,0),
+    e_civil = ifelse(P_022=="1",1,0),
+    raza =  case_when(P_023=="-98" ~0,
+                      P_023=="-99" ~0,
+                      P_023=="3" ~0,
+                      P_023=="4" ~0,
+                      .default = 1),
+    sexo = ifelse(P_018=="2",1,0),
+    educacion = ifelse(as.numeric(P_049)>6,1,0),
+    leer = ifelse(P_039=="1",1,0),
+    hijos = ifelse(as.numeric(P_064)>0,1,0),
+    afiliacion = case_when(P_074=="6"~1,
+                           P_074=="7"~1,
+                           .default = 0),
+    trabajo = ifelse(P_077=="1",1,0),
+    planifica = ifelse(P_073_c=="1",1,0),
+    D = ifelse(P_073_b=="1",1,0),
+    edad = as.numeric(P_021),
+    menor = ifelse(edad<18,1,0),
+    idVivienda = as.numeric(NoForm),
+    idHogar = as.numeric(paste0(idVivienda,sHogar)),
+    idPersona = seq(1,length(NoForm)),
+    hogares =as.numeric(FEH)
+  )
+
+data2021<-data2021 %>% select(variables)
+
+summary(data2021)
+
+writexl::write_xlsx(data2021,"C:/Users/HP-Laptop/OneDrive - Universidad de Antioquia/Maestría en Economía/Evaluación de impacto/Inferencia-Causal/Inferencia_causal/Panel de datos/2021.xlsx")
